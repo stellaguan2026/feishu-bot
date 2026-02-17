0b4192423c55774d979e425a87853f5f22808cdc
stellaguan2026 <stella.guan@bytedance.com>
2026-02-17 18:13:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.post("/feishu", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port", PORT);
 });
